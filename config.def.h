eca646b3b5cfcd9aa8aa2a33609f8eb6a60a398b
lemmarathon <lemmarathon@protonmail.com>
2018-08-25 20:52:28 -0400

    back to top bar

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static const unsigned int systrayspacing = 1;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=6" };
 static const char dmenufont[]       = "monospace:size=6";
 static const char col_gray1[]       = "#222222";
@@ -72,7 +72,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xfce4-terminal", NULL };
 static const char *quitcmd[]  = { "xfce4-session-logout", "-c", NULL };
 static const char *querycmd[]  = { "xfce4-appfinder", "-c", NULL };
